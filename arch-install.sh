fd296cf5057f7d573fe19fac81652eb82102e1c4
Kevin <kevin@chanster.net>
2015-06-27 22:04:38 -0500

    create install function

--- arch-install.sh
+++ arch-install.sh
@@ -1,26 +1,41 @@
 #!/bin/bash
 
+echo "================================================="
 echo "This script installs my Arch Linux Default system"
+echo "================================================="
+echo ""
+echo -n "Proceed with installation? [Y/n] "
+read answer
 
-pacman -Syyu --noconfirm
+function install {
+    # update base install first
+    pacman -Syyu --noconfirm
 
-echo "Installing Desktop and start on tty1"
-pacman -S cinnamon nemo nemo-fileroller
-echo "[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && exec startx" >> ~/.bashrc
+    #Installing Desktop and start on tty1"
+    pacman -S cinnamon nemo nemo-fileroller
+    echo "[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && exec startx" >> ~/.bashrc
 
-pacman -S wget git curl --noconfirm
-# Compression
-pacman -S zip unzip --noconfirm
-# Programming
-pacman -S base-devel python-virtualenv nodejs npm --noconfirm
-# Web browser
-pacman -S chromium 
-yaourt -S chromium-pepper-flash
-# Graphics
-pacman -S tiled
-yaourt -S aseprite
+    pacman -S wget git curl --noconfirm
+    # Compression
+    pacman -S zip unzip --noconfirm
+    # Programming
+    pacman -S base-devel python-virtualenv nodejs npm --noconfirm
+    # Web browser
+    pacman -S chromium
+    yaourt -S chromium-pepper-flash
+    # Graphics
+    pacman -S tiled
+    yaourt -S aseprite
 
-# AUR packages
+    # AUR packages
 
-# Progamming IDEs
-yaourt -S intellij-idea-community-edition atom-editor --noconfirm
+    # Progamming IDEs
+    yaourt -S intellij-idea-community-edition atom-editor --noconfirm
+}
+
+if [[ $answer == "Y" || $answer == "y" || $answer == "" ]]; then
+    install
+else
+    echo "Exiting..."
+    exit 1
+fi
